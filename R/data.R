bd65416670c922fe89b43998b4d6ad11fe0c41ee
Diego Diez <diego10ruiz@gmail.com>
2019-02-20 09:54:40 +0900

    Add documentation for datasets.

---
+++ R/data.R
@@ -0,0 +1,14 @@
+#' Single cell RNA-seq dataset.
+#'
+#' @name dat.expression
+#' @docType data
+#' @keywords data
+NULL
+
+
+#' Single cell tSNE coordingates.
+#'
+#' @name dat.tsne
+#' @docType data
+#' @keywords data
+NULL
