b16f7c10d18ca0f14afeaa110d0223d800402f2d
Diego Diez <diego10ruiz@gmail.com>
2019-03-22 15:03:27 +0900

    Remove SingleCellExperiment and Seurat package dependencies from Imports.

--- R/singleCellHaystack-package.R
+++ R/singleCellHaystack-package.R
@@ -4,8 +4,5 @@
 #' @importFrom grDevices pdf dev.off
 #' @importFrom utils read.csv write.csv
 #' @importFrom splines bs
-#' @importFrom SummarizedExperiment assay
-#' @importFrom SingleCellExperiment reducedDim
-#' @importFrom Seurat GetAssayData Embeddings
 #' @importFrom data.table as.data.table
 "_PACKAGE"
