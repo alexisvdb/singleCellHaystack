d8643d2ac522bde9b9fa9840b3ad7287bc9f9694
Alexis Vandenbon <alexis.vandenbon@gmail.com>
2020-06-03 14:42:29 +0900

    Remove Rtsne from tests, using example dataset instead

--- tests/testthat/test-get_d_kl.R
+++ tests/testthat/test-get_d_kl.R
@@ -1,16 +1,14 @@
 context("test-get_d_kl")
 
-set.seed(123)
-x <- scale(mtcars)
-y <- Rtsne::Rtsne(x, perplexity = 5)$Y
+y <- dat.tsne
 param <- singleCellHaystack:::get_parameters_haystack(y[, 1], y[, 2])
 ref <- singleCellHaystack:::get_reference(param) # reference distribution
-w <- x > 1 # detection
+w <- dat.expression > 1 # detection
 
 z <- sapply(1:5, function(k) {
   singleCellHaystack:::get_D_KL(w[k, ], param, ref$Q, ref$pseudo)
 })
 
 test_that("multiplication works", {
-  expect_equal(z, c(0.5109014, 0.5109014, 0.4967910, 0.4584567, 0.3187935), tolerance = 1e-6)
+  expect_equal(z, c(0.06756395, 0.10501541, 0.12736556, 0.12583667, 0.18401621), tolerance = 1e-6)
 })
--- tests/testthat/test-kde2d_faster.R
+++ tests/testthat/test-kde2d_faster.R
@@ -1,13 +1,12 @@
 context("test-kde2d_faster")
 
-set.seed(123)
-y <- Rtsne::Rtsne(mtcars, perplexity = 10)$Y
+y <- dat.tsne
 param <- singleCellHaystack:::get_parameters_haystack(y[, 1], y[, 2])
 z <- singleCellHaystack:::kde2d_faster(param$dens.x, param$dens.y)
 
 test_that("kde2d_faster works", {
   expect_type(z, "double")
   expect_equal(class(z)[1], "matrix")
-  expect_equal(dim(z), c(40, 51))
+  expect_equal(dim(z), c(43, 41))
   expect_equal(dim(z), param$grid.points)
 })
