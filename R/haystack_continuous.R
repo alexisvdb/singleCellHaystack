451702e4c538ba32eead5e9d49c9ee8161af3f2c
alexisvdb <alexis.vandenbon@gmail.com>
2021-09-03 15:10:24 +0900

    Update haystack_continuous_highD to include sparse matrices, progress bars, etc

--- R/haystack_continuous.R
+++ R/haystack_continuous.R
@@ -29,8 +29,8 @@ haystack_continuous_highD = function(x, expression, grid.points = 100, weights.a
     stop("'x' must be a numeric matrix")
   if(ncol(x) < 2)
     stop("'x' must have at least 2 columns")
-  if(!is.matrix(expression))
-    stop("'expression' must be a matrix")
+  if(!is.matrix(expression) && ! inherits(expression, "lgCMatrix") && ! inherits(expression, "lgRMatrix"))
+    stop("'expression' must be a matrix, lgCMatrix, or lgRMatrix")
   if(ncol(expression) != nrow(x))
     stop("The number of columns in 'expression' must be the same as the rows in 'x'")
   if(!is.numeric(grid.points))
@@ -43,13 +43,20 @@ haystack_continuous_highD = function(x, expression, grid.points = 100, weights.a
     if(length(weights.advanced.Q) != nrow(x))
       stop("The length of 'weights.advanced.Q' must be the same as the number of rows in 'x'")
   }
-  if(!is.logical(scale) | length(scale) > 1)
-    stop("The value of 'scale' must be either TRUE or FALSE")
-  if(grid.method!="centroid" & grid.method!="seeding")
-    stop("The value of 'grid.method' must be either 'centroid' or 'seeding'")
+
+  # if expression is a lgCMatrix, convert it to a lgRMatrix
+  if(inherits(expression, "lgCMatrix")){
+    message("### converting expression data from lgCMatrix to lgRMatrix")
+    expression <- as(expression, "RsparseMatrix")
+  }
 
   count.cells <- ncol(expression)
   count.genes <- nrow(expression)
+  if(n.genes.to.randomize > count.genes){
+    warning("Number of genes to randomize (",n.genes.to.randomize,") is higher than the number of genes in the data.")
+    warning("Seeting number of genes to randomize to ",count.genes)
+    n.genes.to.randomize <- count.genes
+  }
 
   # warn about unusal input sizes
   if(nrow(x) < 50)
@@ -63,6 +70,18 @@ haystack_continuous_highD = function(x, expression, grid.points = 100, weights.a
   if(grid.points > count.cells/10)
     warning("The value of 'grid.points' appears to be very high (> No. of cells / 10). You can set the number of grid points using the 'grid.points' parameter.")
 
+  #### NOT SURE IF NEEDED - REMOVE IF NO ####
+  # advanced sampling is slow on large datasets. Recommend using wrswoR
+  if(!is.null(use.advanced.sampling)){
+    if(requireNamespace("wrswoR", quietly = TRUE)){
+      use.wrswoR <- TRUE
+      message("### Using package wrswoR to speed up random sampling")
+    } else {
+      use.wrswoR <- FALSE
+      message("### You are running advanced sampling. Installing the package \"wrswoR\" might result in much shorter runtimes.")
+    }
+  }
+  #### END NOT SURE IF NEEDED
 
   # scale data if needed
   if(scale){
@@ -124,12 +143,19 @@ haystack_continuous_highD = function(x, expression, grid.points = 100, weights.a
   # get D_KL (or relative entropy) of this P vs reference Q
   message("### calculating Kullback-Leibler divergences...")
   D_KL.observed <- rep(0,count.genes)
-  for(i in 1:count.genes){
-    D_KL.observed[i] <- get_D_KL_continuous_highD(weights=expression[i,], density.contributions = density.contributions, reference.prob = Q, pseudo = pseudo)
-
-    if(i%%1000==0)
-      message(paste0("### ... ",i," rows out of ",count.genes," done"))
+  pb <- txtProgressBar(min = 0, max = count.genes, style = 3, file = stderr()) # progress bar
+  if(is.matrix(expression)){
+    for(i in 1:count.genes){
+      D_KL.observed[i] <- get_D_KL_continuous_highD(weights=expression[i,], density.contributions = density.contributions, reference.prob = Q, pseudo = pseudo)
+      setTxtProgressBar(pb, i) # progress bar
+    }
+  } else if(inherits(expression, "lgRMatrix")){
+    for(i in 1:count.genes){
+      D_KL.observed[i] <- get_D_KL_continuous_highD(weights=extract_row_lgRMatrix(expression,i), density.contributions = density.contributions, reference.prob = Q, pseudo = pseudo)
+      setTxtProgressBar(pb, i) # progress bar
+    }
   }
+  close(pb) # progress bar
 
 
   ### use randomization to estimate expected values
@@ -144,9 +170,7 @@ haystack_continuous_highD = function(x, expression, grid.points = 100, weights.a
   expr.sd   <- apply(expression,1,sd) + 1e-300
   coeffVar  <- expr.sd/expr.mean # coefficient of variation
 
-  message("### starting randomizations...")
-
-  ### NEED TO ADD CHECK IF THE NUMBER OF GENES TO RANDOMIZE IS SMALLER THAN TOTAL NUMBER OF GENES IN DATA!!
+  message("### performing randomizations...")
 
   o <- order(coeffVar)
   genes.to.randomize <- o[floor(seq(1,count.genes, length.out = n.genes.to.randomize))]
@@ -159,9 +183,10 @@ haystack_continuous_highD = function(x, expression, grid.points = 100, weights.a
   # to store randomization results
   all.D_KL.randomized <- matrix(NA,nrow=n.genes.to.randomize, ncol=randomization.count)
 
+  pb <- txtProgressBar(min = 0, max = T.counts.to.select, style = 3, file = stderr()) # progress bar
+
   for(i in 1:n.genes.to.randomize){
-    if(i%%10==0)
-      message(paste0("### ... ",i," sets out of ",n.genes.to.randomize," done"))
+    setTxtProgressBar(pb, i) # progress bar
 
     D_KL.randomized <- rep(NA,randomization.count)
     vector.to.randomize <- expression[genes.to.randomize[i],]
@@ -175,7 +200,7 @@ haystack_continuous_highD = function(x, expression, grid.points = 100, weights.a
     }
     all.D_KL.randomized[i,] <- D_KL.randomized
   }# end for all T counts to select
-
+  close(pb) # progress bar
 
 
   message("### estimating p-values...")
@@ -187,6 +212,12 @@ haystack_continuous_highD = function(x, expression, grid.points = 100, weights.a
   # bonferroni correction for multiple testing
   p.adjs <- p.vals + log10(length(p.vals))
   p.adjs[p.adjs>0] <- 0 # p values should be at most 1; so log10 should be <= 0
+
+  if(!is.null(dir.randomization)){
+    message("### writing randomized Kullback-Leibler divergences to file...")
+    outputfile.randomized.D_KL <- paste0(dir.randomization,"/random.D_KL.csv")
+    write.csv(file=outputfile.randomized.D_KL,all.D_KL.randomized)
+  }
 
   # prepare grid coordinates to return
   # if input data was scaled, the grid points have to be re-scaled
