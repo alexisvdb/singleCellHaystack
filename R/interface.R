ec5e9566b15b84d7a961f31062050938e4d8edb8
alexisvdb <alexis.vandenbon@gmail.com>
2021-09-13 10:41:28 +0900

    first attempt at interface to all haystack functions

---
+++ R/interface.R
@@ -0,0 +1,55 @@
+
+haystack_interface = function(coordinates, expression = NULL, detection = NULL,
+                              type = NULL,...){
+  # I think we can change the name of this function to just "haystack" or "singleCellHaystack"
+  # we might have to do something about the s3.R function "haystack" in that case, to avoid confusion
+
+  # might add several checks on the input here?
+  # in that case, we can remove redundant, general checks from all the functions
+  # and keep only specific checks inside the individual functions
+
+  if(type == "highD" && !is.null(expression)){
+    # highD continuous
+    message("### calling haystack version highD continuous...")
+    haystack_continuous_highD(x = coordinates, expression = expression)
+
+  } else if(type == "highD" && !is.null(detection)){
+    # highD binary
+    message("### calling haystack version highD binary...")
+    haystack_highD(x = coordinates, detection = detection)
+
+  } else if(type == "2D" && !is.null(expression)){
+    # 2D continuous
+    message("### calling haystack version 2D continuous...")
+    haystack_continuous_2D(x = coordinates[,1], y = coordinates[,2], expression = expression)
+
+  } else if(type == "2D" && !is.null(detection)){
+    # 2D binary
+    message("### calling haystack version 2D binary...")
+    haystack_2D(x = coordinates[,1], y = coordinates[,2], detection = detection)
+
+  } else if(type == "spatial" && !is.null(expression)){
+    # spatial continuous
+    message("### calling haystack version spatial continuous...")
+    haystack_continuous_spatial(coordinates = coordinates, expression = expression)
+
+  } else if(type == "spatial" && !is.null(detection)){
+    # sorry: no such function!
+    # and no plan to make it, really
+    message("### there is no function to analyze spatial data using non-continuous (binary) expression values...")
+
+  } else {
+    # print some info about usage
+    message("### usage:\n\n",
+    "    haystack_interface(coordinates, [expression or detection data], type)\n\n",
+    "    where:\n",
+    "    - coordinates: typically >2D coordinates of cells in the PC space (2D is also fine), \n",
+    "                   or 2D/3D spatial coordinates\n",
+    "    - expression : matrix or data.frame with continuous expression values of genes in cells/spots\n",
+    "    - detection  : matrix or data.frame with binary (TRUE or FALSE) detection values \n",
+    "                   of genes in cells/spots\n",
+    "    - type       : highD, 2D, or spatial, depending on the type of input coordinates\n\n")
+  }
+
+}
+
