92c8048075f57786f02bafe0578911d4dfcc655e
Diego Diez <diego10ruiz@gmail.com>
2022-03-24 15:08:51 +0900

    Fix tests in test-haystack_continuous.R.

--- tests/testthat/test-haystack_coninuous.R
+++ tests/testthat/test-haystack_coninuous.R
@@ -11,7 +11,7 @@ test_that("haystack continuous 2D works", {
   expect_equal(class(x$results), "data.frame")
   expect_equal(dim(x$results), c(500, 3))
   expect_equal(x$results["gene_24", "D_KL"], 1.74568845, tolerance = 1e-6)
-  expect_equal(x$results["gene_24", "log.p.vals"], -7.151125e+00, tolerance = 1e-6)
+  expect_equal(x$results["gene_24", "log.p.vals"], -7.79657, tolerance = 1e-6)
 })
 
 x <- haystack_continuous_highD(x = dat.tsne, expression = dat.expression, grid.points = 60)
@@ -24,8 +24,8 @@ test_that("haystack continuous highD works", {
   expect_equal(dim(x$results), c(500, 3))
   expect_equal(class(x$info$grid.coordinates)[1], "matrix")
   expect_equal(dim(x$info$grid.coordinates), c(60, 2))
-  expect_equal(x$results["gene_24", "D_KL"], 2.557407, tolerance = 1e-6)
-  expect_equal(x$results["gene_24", "log.p.vals"], -26.17737, tolerance = 1e-6)
+  expect_equal(x$results["gene_24", "D_KL"], 2.459008, tolerance = 1e-6)
+  expect_equal(x$results["gene_24", "log.p.vals"], -28.16831, tolerance = 1e-6)
 })
 
 x <- haystack_continuous_highD(x = dat.tsne, expression = dat.expression, grid.method = "seeding", grid.points = 60)
@@ -38,6 +38,6 @@ test_that("haystack continuous highD works", {
   expect_equal(dim(x$results), c(500, 3))
   expect_equal(class(x$info$grid.coordinates)[1], "matrix")
   expect_equal(dim(x$info$grid.coordinates), c(60, 2))
-  expect_equal(x$results["gene_24", "D_KL"], 2.507272, tolerance = 1e-6)
-  expect_equal(x$results["gene_24", "log.p.vals"], -23.50283, tolerance = 1e-6)
+  expect_equal(x$results["gene_24", "D_KL"], 2.448916, tolerance = 1e-6)
+  expect_equal(x$results["gene_24", "log.p.vals"], -22.1835, tolerance = 1e-6)
 })
