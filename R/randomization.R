1e32cb6c222716758426810523cf247ebf344314
Diego Diez <diego10ruiz@gmail.com>
2022-06-17 11:59:32 +0900

    Improve labels for x-axis in plot_rand_fit().

--- R/randomization.R
+++ R/randomization.R
@@ -20,12 +20,22 @@ plot_rand_fit.haystack <- function(x, type=c("mean", "sd")) {
   df <- d$cv.selected["df"]
   rmsd <- format(d$cv.selected["rmsd"], digits=2)
 
+  method <- strsplit(x$info$method, "_")[[1]]
+
+  if (method[1] == "continuous")
+    xlab <- "logCV"
+
+  if (method[1] == "binary")
+    xlab <- "# positive cells"
+
+  ylab <- paste0(type, " of logKLD")
+
   ggplot(d$observed, aes(.data[["x"]], .data[["y"]])) +
     geom_point() +
     geom_line(color="red", data=d$fitted) +
     labs(
-      x = "points",
-      y = type,
+      x = xlab,
+      y = ylab,
       title=paste0(x$info$method, ": fitted values for ", type),
       subtitle=paste0("degree: ", degree, ", df: ", df, ", rmsd: ", rmsd))
 }
